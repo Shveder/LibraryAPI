a326f55dbc8e12e0b4605bfee9ec7271831a3805
Shveder <shvedegor7687@gmail.com>
2024-08-26 16:20:47 +0300

    Add master: add DataContext.cs

---
+++ Library.Infrastructure/DatabaseContext/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Library.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Infrastructure.DatabaseContext;
+
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Author> Authors { get; set; }
+}
