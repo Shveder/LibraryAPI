924533ef49632d39694cc8ebbedc7d6b1496be66
Shveder <shvedegor7687@gmail.com>
2024-08-26 16:20:37 +0300

    Add master: add ServiceConfig.cs

---
+++ Library.Infrastructure/ServiceConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Library.Infrastructure.Services.Base;
+
+namespace Library.Infrastructure;
+
+public static class ServiceConfig
+{
+    public static void RegisterService(IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        var serviceInterfaces = assembly.GetTypes()
+            .Where(type => type.IsInterface &&
+                           type.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IBaseService<,>)))
+            .ToList();
+
+        foreach (var serviceInterface in serviceInterfaces)
+        {
+            var serviceImplementation = assembly
+                .GetTypes()
+                .FirstOrDefault(type => type is { IsClass: true, IsAbstract: false } && serviceInterface.IsAssignableFrom(type));
+
+            if (serviceImplementation is not null)
+                services.AddScoped(serviceInterface, serviceImplementation);
+        }
+    }
+}
