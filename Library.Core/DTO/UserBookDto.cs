b22c46d97613d1372d68da6902e8d151102411eb
Shveder <shvedegor7687@gmail.com>
2024-08-27 15:31:27 +0300

    Add master: add UserBookDto.cs

---
+++ Library.Core/DTO/UserBookDto.cs
@@ -0,0 +1,11 @@
+﻿using Library.Core.DTO.Base;
+
+namespace Library.Core.DTO;
+
+public class UserBookDto : BaseDto
+{
+    public Guid UserId { get; set; }
+    public Guid BookId { get; set; }
+    public DateTime DateTaken { get; set; }
+    public DateTime DateReturn { get; set; }
+}
