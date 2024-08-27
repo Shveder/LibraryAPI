91bde2b82738701ec098c29ed80a175938de3faa
Shveder <shvedegor7687@gmail.com>
2024-08-27 15:31:35 +0300

    Add master: add UserBookProfile.cs

---
+++ Library.Infrastructure/Mappings/UserBookProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Library.Core.DTO;
+using Library.Core.Models;
+
+namespace Library.Infrastructure.Mappings;
+
+public class UserBookProfile : Profile
+{
+    public UserBookProfile()
+    { 
+        CreateMap<UserBookDto, UserBook>(); 
+        CreateMap<UserBook, UserBookDto>();
+    }
+}
