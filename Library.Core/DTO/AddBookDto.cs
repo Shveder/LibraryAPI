94d4cbc65a64ba73d196a8af1bff8b054d665f79
Shveder <shvedegor7687@gmail.com>
2024-08-27 17:20:40 +0300

    Add master: add AddBookDto.cs

---
+++ Library.Core/DTO/AddBookDto.cs
@@ -0,0 +1,29 @@
+﻿namespace Library.Core.DTO;
+
+public class AddBookDto
+{
+    /// <summary>
+    /// ISBN of book
+    /// </summary>
+    public string ISBN { get; set; }
+    /// <summary>
+    /// Name of book
+    /// </summary>
+    public string BookName { get; set; }
+    /// <summary>
+    /// Genre of book
+    /// </summary>
+    public string Genre { get; set; }
+    /// <summary>
+    /// Description of book
+    /// </summary>
+    public string Description { get; set; }
+    /// <summary>
+    /// Date when book was taken
+    /// </summary>
+    public DateTime DateTaken { get; set; }
+    /// <summary>
+    /// Author of book
+    /// </summary>
+    public Guid AuthorId { get; set; }
+}
