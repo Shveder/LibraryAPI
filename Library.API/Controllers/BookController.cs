d7465ba98b7b32e157342e03726a9d5d65021ca9
Shveder <shvedegor7687@gmail.com>
2024-08-27 17:56:02 +0300

    Add master: add getting book by isbn

--- Library.API/Controllers/BookController.cs
+++ Library.API/Controllers/BookController.cs
@@ -1,4 +1,5 @@
 ﻿using Library.API.Controllers.Base;
+using Library.Common;
 using Library.Core.DTO;
 using Library.Core.Models;
 using Library.Infrastructure.Services;
@@ -13,4 +14,20 @@ namespace Library.API.Controllers;
 [Route("api/Book")]
 [ApiController]
 public class BookController (IBookService bookService)
-    : BaseController<IBookService, Book, BookDto>(bookService);
+    : BaseController<IBookService, Book, BookDto>(bookService){
+    private readonly IBookService _bookService = bookService;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="isbn"></param>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<IActionResult> GetByIsbnAsync(string isbn)
+    {
+        var entity = await _bookService.GetByIsbnAsync(isbn);
+        
+        return Ok(new ResponseDto<BookDto>(CommonStrings.SuccessResult, data: entity));
+    }
+}
+
