fdf13d2d6d76950bff9a169201c2e5324df4a960
Shveder <shvedegor7687@gmail.com>
2024-08-27 13:03:39 +0300

    Edit master: edit DataContext

--- Library.Infrastructure/DatabaseContext/DataContext.cs
+++ Library.Infrastructure/DatabaseContext/DataContext.cs
@@ -7,4 +7,11 @@ public class DataContext(DbContextOptions<DataContext> options) : DbContext(opti
 {
     public DbSet<Book> Books { get; set; }
     public DbSet<Author> Authors { get; set; }
+    public DbSet<User> Users { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(
+            @"Host=localhost;Port=5432;Database=library;Username=postgres;Password=postgres");
+    }
 }
