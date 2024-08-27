400358d361e8e7fb8b6521ee745cdff3907c6fd5
Shveder <shvedegor7687@gmail.com>
2024-08-27 13:00:43 +0300

    Add master: add UserBooks.cs

---
+++ Library.Core/Models/UserBooks.cs
@@ -0,0 +1,23 @@
+﻿using Library.Core.Models.Base;
+
+namespace Library.Core.Models;
+
+public class UserBooks : BaseModel
+{
+    /// <summary>
+    /// User who took the book
+    /// </summary>
+    public User User { get; set; }
+    /// <summary>
+    /// Book that was taken
+    /// </summary>
+    public Book Book { get; set; }
+    /// <summary>
+    /// Date when book was taken
+    /// </summary>
+    public DateTime DateTaken { get; set; }
+    /// <summary>
+    /// The date the book is due to be returned
+    /// </summary>
+    public DateTime DateReturn { get; set; }
+}
