23722454a07e9d75363b26e5ddd931e1a0915ec7
Shveder <shvedegor7687@gmail.com>
2024-08-27 17:21:02 +0300

    Add master: override post method

--- Library.Infrastructure/Services/AuthorService.cs
+++ Library.Infrastructure/Services/AuthorService.cs
@@ -1,7 +1,9 @@
 ﻿using Library.Core.DTO;
 using Library.Core.Models;
 using Library.Infrastructure.DatabaseContext;
+using Library.Infrastructure.Exceptions;
 using Library.Infrastructure.Services.Base;
+using Microsoft.EntityFrameworkCore;
 
 namespace Library.Infrastructure.Services;
 
@@ -11,4 +13,28 @@ using AutoMapper;
 
 [AutoInterface(Inheritance = [typeof(IBaseService<AuthorDto, Author>)])]
 public class AuthorService(DataContext dbContext, IMapper mapper)
-    : BaseService<AuthorDto, Author>(dbContext, mapper), IAuthorService;
+    : BaseService<AuthorDto, Author>(dbContext, mapper), IAuthorService
+{
+    public override async Task<AuthorDto> PostAsync(AuthorDto dto)
+    {
+        if (await IsAuthorUnique(dto.Name))
+            throw new IncorrectDataException("Author with this name already exists");
+        
+        var author = new Author()
+        {
+            Name = dto.Name,
+            Surname = dto.Surname,
+            Birthday = dto.Birthday,
+            Country = dto.Country
+        };
+        
+        await Context.AddAsync(author);
+        await DbContext.SaveChangesAsync();
+        return dto;
+    }
+    private async Task<bool> IsAuthorUnique(string name)
+    {
+        var author = await DbContext.Set<Author>().FirstOrDefaultAsync(model => model.Name == name); 
+        return author != null;
+    }
+}
