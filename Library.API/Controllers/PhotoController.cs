fa67b0d23c0310ea040723fb4750cdd5ed99702a
Shveder <shvedegor7687@gmail.com>
2024-08-28 13:52:47 +0300

    Add master: add photo handling

---
+++ Library.API/Controllers/PhotoController.cs
@@ -0,0 +1,24 @@
+﻿using Library.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PhotoController(IPhotoService photoService)
+    : ControllerBase
+{
+    [HttpPost("HandleFileUpload/{bookId}")]
+    public async Task<IActionResult> HandleFileUpload([FromRoute] Guid bookId, IFormFile file)
+    {
+        await photoService.SavePhotoAsync(bookId, file.OpenReadStream());
+        return Ok("Uploaded successfully");
+    }
+
+    [HttpGet("GetPhoto/{bookId}")]
+    public async Task<IActionResult> GetPhoto([FromRoute] Guid bookId)
+    {
+        var photoStream = await photoService.GetPhotoAsync(bookId);
+        return File(photoStream, "image/jpeg");
+    }
+}
---
+++ Library.Infrastructure/Services/PhotoService.cs
@@ -0,0 +1,58 @@
+﻿using AutoInterfaceAttributes;
+using Microsoft.Extensions.Logging;
+
+namespace Library.Infrastructure.Services;
+
+[AutoInterface]
+public class PhotoService(ILogger<PhotoService> logger) : IPhotoService
+{
+    private readonly string _rootLocation = "uploads";
+
+        public async Task SavePhotoAsync(Guid bookId, Stream fileStream)
+        {
+            try
+            {
+                string location =  $"books/{bookId}";
+                string bookDir = Path.Combine(_rootLocation, location);
+                Directory.CreateDirectory(bookDir);
+
+                string filePath = Path.Combine(bookDir, $"{bookId}.jpg");
+                
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                }
+                
+                using (var file = File.Create(filePath))
+                {
+                    await fileStream.CopyToAsync(file);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Cant save photo for book {bookId}: {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task<Stream> GetPhotoAsync(Guid bookId)
+        {
+            try
+            {
+                string location = $"books/{bookId}";
+                string filePath = Path.Combine(_rootLocation, location, $"{bookId}.jpg");
+
+                if (!File.Exists(filePath))
+                {
+                    filePath = Path.Combine(_rootLocation, "profileIcon.png");
+                }
+                
+                return await Task.FromResult(File.OpenRead(filePath));
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Cant get photo for book {bookId}: {ex.Message}");
+                throw;
+            }
+        }
+}
