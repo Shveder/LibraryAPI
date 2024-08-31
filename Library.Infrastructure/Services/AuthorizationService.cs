c3498bc778da5d01b210920f44135e3eec8aaea0
Shveder <shvedegor7687@gmail.com>
2024-08-31 21:17:00 +0300

    Edit: master - edit jwt creating

--- Library.Infrastructure/Services/AuthorizationService.cs
+++ Library.Infrastructure/Services/AuthorizationService.cs
@@ -17,15 +17,15 @@ namespace Library.Infrastructure.Services;
 
 [AutoInterface]
 public class AuthorizationService(DataContext context, IConfiguration configuration,
-    ILogger<AuthorizationService>logger, IDbRepository repository) : IAuthorizationService
+    ILogger<AuthorizationService> logger, IDbRepository repository) : IAuthorizationService
 {
     public async Task<User> Login(string login, string password)
     {
         var user = await repository.Get<User>(model => model.Login == login).FirstOrDefaultAsync();
-        
+
         if (user == null)
             throw new IncorrectDataException("Invalid login or password");
-        
+
         password = Hash(password);
         password = Hash(password + user.Salt);
 
@@ -37,22 +37,22 @@ public class AuthorizationService(DataContext context, IConfiguration configurat
 
         return user1;
     }
-    
+
     public async Task Register(RegisterUserRequest request)
     {
         if (request.Password != request.PasswordRepeat)
             throw new IncorrectDataException("Passwords do not match");
-        if(await IsLoginUnique(request.Login))
+        if (await IsLoginUnique(request.Login))
             throw new IncorrectDataException("There is already a user with this login in the system");
-        if(request.Login.Length is < 4 or > 32)
+        if (request.Login.Length is < 4 or > 32)
             throw new IncorrectDataException("Login length must be between 4 and 32 characters.");
-        if(request.Password.Length is < 4 or > 32)
+        if (request.Password.Length is < 4 or > 32)
             throw new IncorrectDataException("Password length must be between 4 and 32 characters.");
-        
+
         request.Password = Hash(request.Password);
         string salt = GetSalt();
         request.Password = Hash(request.Password + salt);
-        
+
         var user = new User
         {
             Login = request.Login,
@@ -64,11 +64,13 @@ public class AuthorizationService(DataContext context, IConfiguration configurat
         await repository.SaveChangesAsync();
         logger.LogInformation($"User created (Login: {request.Login})");
     }
+
     private async Task<bool> IsLoginUnique(string login)
     {
-        var user = await repository.Get<User>(model => model.Login == login).FirstOrDefaultAsync(); 
+        var user = await repository.Get<User>(model => model.Login == login).FirstOrDefaultAsync();
         return user != null;
     }
+
     private string GetSalt()
     {
         byte[] salt = new byte[16];
@@ -92,16 +94,17 @@ public class AuthorizationService(DataContext context, IConfiguration configurat
             return sb.ToString();
         }
     }
+
     public async Task<string> GenerateTokenAsync(string login, string password)
     {
         var user1 = await context.Set<User>().FirstOrDefaultAsync(model => model.Login == login);
-        
+
         if (user1 == null)
             throw new IncorrectDataException("Invalid login or password");
-        
+
         password = Hash(password);
         password = Hash(password + user1.Salt);
-        
+
         var user = await repository.Get<User>(u =>
                 u.Login == login && u.Password == password).FirstOrDefaultAsync();
 
@@ -112,28 +115,19 @@ public class AuthorizationService(DataContext context, IConfiguration configurat
 
         var claims = new List<Claim>
         {
-            new Claim(JwtRegisteredClaimNames.Sub, configuration["Jwt:Subject"]),
-            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-            new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
-            new Claim("id", user.Id.ToString()),
-            new Claim("name", user.Login),
-            new Claim("role", user.Role) 
+            new ("id", user.Id.ToString()),
+            new ("name", user.Login),
+            new ("role", user.Role)
         };
+        
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"] ?? string.Empty));
+        var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-      
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
-        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-        var expiry = DateTime.UtcNow.AddMinutes(10);
-
-        var token = new JwtSecurityToken(
-            issuer: configuration["Jwt:Issuer"],
-            audience: configuration["Jwt:Audience"],
-            claims: claims,
-            expires: expiry,
-            signingCredentials: creds
-        );
-
-        return new JwtSecurityTokenHandler().WriteToken(token);
+        return new JwtSecurityTokenHandler().WriteToken(new JwtSecurityToken(
+            configuration["Jwt:Issuer"],
+            configuration["Jwt:Audience"],
+            claims,
+            expires: DateTime.UtcNow.AddDays(1),
+            signingCredentials: signIn));
     }
-
 }
